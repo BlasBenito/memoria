a384544563bd13b2f1ded3db8d630b0dc6817677
blas <blasbenito@gmail.com>
2026-02-10 09:30:14 +0100

    - fixed cran release issues

--- dev_scripts/check_as_cran.sh
+++ dev_scripts/check_as_cran.sh
@@ -4,7 +4,7 @@ export _R_CHECK_FORCE_SUGGESTS_=TRUE
 
 
 #enforcing NO CRAN to avoid running tests
-NOT_CRAN="" R CMD check --as-cran --timings /home/blas/Dropbox/blas/GITHUB/R_packages/memoria_1.1.0.tar.gz
+R CMD check --as-cran --timings /home/blas/Dropbox/blas/GITHUB/R_packages/memoria_1.1.0.tar.gz
 
 #example timings
 R CMD check --as-cran /home/blas/Dropbox/blas/GITHUB/R_packages/collinear_3.0.0.tar.gz
